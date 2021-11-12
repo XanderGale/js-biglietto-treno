7c5eb82975553f3e9391eaf1ffdf9090d85267e2
XanderGale <alessandro.galeazzi@icloud.com>
2021-11-12 17:40:15 +0100

    Completed

--- js/script.js
+++ js/script.js
@@ -13,6 +13,29 @@ console.log(userAge, travelKm, euroAlKm);
 
 // Calcolo prezzo biglietto in base ai km
 
-let priceKm = travelKm * euroAlKm;
+let price = travelKm * euroAlKm;
 
-console.log (priceKm);
+console.log (price);
+
+// Dichiaro condizioni sconto
+
+if (userAge > 65) {
+    let priceOutput = price - ((price / 100) * 40 );
+    console.log(priceOutput);
+
+    // Creo il messaggio di Output
+    alert(`Il prezzo del tuo biglietto sarà: ${priceOutput}`);
+} else if ( userAge < 18 ) {
+    let priceOutput = price - ((price / 100) * 20);
+    console.log(priceOutput);
+
+    // Creo il messaggio di Output
+    alert(`Il prezzo del tuo biglietto sarà: ${priceOutput}`);
+} else {
+    priceOutput = price;
+    console.log(priceOutput);
+
+    // Creo il messaggio di Output
+    alert(`Il prezzo del tuo biglietto sarà: ${priceOutput} / €`);
+}
+
